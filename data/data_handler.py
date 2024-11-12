e62abd0744abac75113b7334d734061e7675026d
153996442+l-hary@users.noreply.github.com <153996442+l-hary@users.noreply.github.com>
2024-11-13 00:27:32 +0100

    feat: update plot styling

--- data/data_handler.py
+++ data/data_handler.py
@@ -14,12 +14,18 @@ def load_and_preprocess_data(file_path: str) -> pd.DataFrame:
     # Using Excel file, as csv from KSH have encoding issues
     try:
         df = pd.read_excel(file_path, skiprows=1)
-        df = df.drop(df.index[0:7])
-        df = df.drop(df.index[-1])
+        df = drop_unnecessary_columns(file_path, df)
+        df = df.rename(columns={"Lakásállomány, január 1.": "Lakásállomány"})
     except FileNotFoundError:
         raise FileNotFoundError("The specified file was not found.")
     except pd.errors.EmptyDataError:
         raise ValueError("The file is empty.")
     except Exception as e:
         raise ValueError(f"An error occurred while loading the data: {e}")
+    return df
+
+
+def drop_unnecessary_columns(file_path: str, df: pd.DataFrame) -> pd.DataFrame:
+    df = df.drop(df.index[0:7])
+    df = df.drop(df.index[-1])
     return df
--- visualization/chart_generator.py
+++ visualization/chart_generator.py
@@ -23,12 +23,16 @@ def generate_lr_chart(
     """
     if ax is None:  # creates a new ax if not provided
         ax = plt.gca()
-    ax.scatter(x, y, color="blue", label="Actual Data")
-    ax.plot(x, prediction, color="red", label="Regression Line")
-    ax.set_xlabel("Transactions")
-    ax.set_ylabel("Loans")
+    ax.scatter(x, y, color="blue", label="Adatpontok")
+    ax.plot(x, prediction, color="red", label="Regressziós egyenes")
+
+    # Format axes
+    ax.set_xlabel("Lakáspiaci tranzakciók száma")
+    ax.set_ylabel("Folyósított lakáshitelek száma")
+    ax.get_yaxis().set_major_formatter(plt.FuncFormatter(lambda x, _: f"{x:,.0f}"))
+    ax.get_xaxis().set_major_formatter(plt.FuncFormatter(lambda x, _: f"{x:,.0f}"))
     ax.legend()
-    # TODO add labels and title
+    # TODO add title
     return ax
 
 
@@ -36,13 +40,28 @@ def generate_line_chart(x: pd.Series, y: pd.Series, ax: plt.Axes = None) -> plt.
     if ax is None:
         ax = plt.gca()  # creates a new ax if not provided
     x = x.astype(str)
-    sns.lineplot(x=x, y=y, ax=ax)
-    ax.set_xticklabels(
-        ax.get_xticklabels(), rotation=45
-    )  # Rotate for better readability
-    # TODO add labels and title
-    # TODO format y axis numbers
 
+    sns.lineplot(x=x, y=y, ax=ax, linestyle="--", marker="o", color="salmon")
+    ax.set_xticklabels(ax.get_xticklabels(), rotation=45)
+
+    # Set fixed stepping for y-axis
+    ax.yaxis.set_major_locator(plt.MultipleLocator(100000))
+    ax.set_ylim(bottom=4200000, top=4700000)
+    # TODO add labels and title
+
+    # Format y-axis numbers
+    ax.get_yaxis().set_major_formatter(plt.FuncFormatter(lambda x, _: f"{x:,.0f}"))
+
+    # Add callouts (annotations)
+    for i in range(len(x)):
+        ax.annotate(
+            text=f"{y.iloc[i]/1000000:,.2f}M",
+            xy=(x.iloc[i], y.iloc[i]),
+            textcoords="offset points",
+            xytext=(0, 5),
+            ha="center",
+            rotation=50,
+        )
     return ax
 
 
