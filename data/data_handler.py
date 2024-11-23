aa961cccb8f72d713f70fdd5c89ee0810e7bb932
l-hary <153996442+l-hary@users.noreply.github.com>
2024-11-23 21:41:43 +0100

    feat: add year on year change calculator

--- data/data_handler.py
+++ data/data_handler.py
@@ -39,3 +39,40 @@ def _drop_unnecessary_columns(file_path: str, df: pd.DataFrame) -> pd.DataFrame:
     df = df.drop(df.index[0:7])
     df = df.drop(df.index[-1])
     return df
+
+
+def add_year_on_year_change(
+    df: pd.DataFrame, target: str | list, result: str | list = None
+) -> None:
+    """
+    Add year-on-year change columns to the DataFrame.
+
+    Parameters:
+    df (pd.DataFrame): The DataFrame to modify.
+    target (str | list): The target column(s) to calculate the year-on-year change for.
+    result (str | list, optional): The name(s) of the result column(s). Defaults to None.
+
+    Returns:
+    None: The function modifies the DataFrame in place.
+    """
+
+    def add_default_suffix(df, target):
+        df[f"{target} változása az előző évhez képest"] = df[target] - df[target].shift(
+            1
+        )
+
+    # checks if a single column name was given
+    if isinstance(target, str):
+        if isinstance(result, str):  # checks if result column name was provided
+            df[result] = df[target] - df[target].shift(1)
+        else:  # generates column with default name
+            add_default_suffix(df, target)
+
+    if isinstance(target, list):  # checks if multiple column names were given
+        # Checks if the length of the target list and result list matches for renaming
+        if isinstance(result, list) and len(target) == len(result):
+            for index, column in enumerate(target):
+                df[result[index]] = df[column] - df[column].shift(1)
+        else:  # generates columns with default names
+            for column in target:
+                add_default_suffix(df, column)
